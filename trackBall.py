f602a41a0b8c3ec47fd1832c97fc5be35beab0a7
Mark <halonen.mark@gmail.com>
2016-02-07 18:25:18 -0500

    Basic masking and contour finding

--- trackBall.py
+++ trackBall.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 import os
 import numpy as np
 import ntpath
+import imutils
 import tkinter as tk
 root = tk.Tk()
 screen_width = root.winfo_screenwidth()
@@ -20,18 +21,38 @@ def displayImage(img, windowName = "Image"):
     cv2.imshow(windowName,temp)
     cv2.waitKey(0)
 
-imagesDir = "/Users/Mark/Google Drive/BroomballAlgo/images"
-maskDir = "/Users/Mark/Google Drive/BroomballAlgo/silverRinkMask.png"
+imagesDir = "images"
+maskDir = "silverRinkMask.png"
 mask = cv2.imread(maskDir, 0)
+ballSizeInches = 5
+inchToPixelRatio = .1
 
 for file in os.listdir(imagesDir):
+    colorIm = cv2.imread(imagesDir + "/" + file)
     im = cv2.imread(imagesDir + "/" + file,0)
     height, width = im.shape[:2]
     mask = cv2.resize(mask, (width, height))
     res = cv2.bitwise_and(im,im,mask = mask)
 
     ret2,thres = cv2.threshold(res,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+    (cnts, _) = cv2.findContours(thres.copy(), cv2.RETR_TREE,
+		cv2.CHAIN_APPROX_SIMPLE)
+
+    print str(len(cnts)) + " :num contours"
+    #loop over the contours
+    for c in cnts:
+		# if the contour is too large, ignore it
+		if cv2.contourArea(c) > 150 or cv2.contourArea(c) < 10:      
+		 	continue
+
+		# compute the bounding box for the contour, draw it on the frame,
+		# and update the text
+		(x, y, w, h) = cv2.boundingRect(c)
+		cv2.rectangle(colorIm, (x, y), (x + w, y + h), (0, 255, 0), 5)
+
+
     #ret2,thres = cv2.threshold(res,0,255,204)
-    displayImage(thres)
+    displayImage(colorIm)
     #plt.imshow(thres),plt.show()
 
